5c1332572f14011cfb78f1cc2d0492b9c6934516
kekavigi <kekavigi@protonmail.com>
2025-07-31 05:31:16 +0000

    kembangkan sketsa tampilan quiz
    FossilOrigin-Name: 2fee276a1745dec1f048f5cb6eb2b74b4f2cc4bb86651f419a4a4ad4aa27f631

---
+++ static/quiz.js
@@ -0,0 +1,75 @@
+import { Chess } from './libs/chess.js';
+import { Chessboard, INPUT_EVENT_TYPE } from "./cb/Chessboard.js"
+import { PromotionDialog } from "./cb/extensions/promotion-dialog/PromotionDialog.js"
+import { Markers } from "./cb/extensions/markers/Markers.js"
+
+var ws = new WebSocket("ws://localhost:8000/ws", ["Authorization", "token"]);
+var data = {};
+var counter = 0;
+var game = new Chess();
+var board = new Chessboard(document.getElementById("myBoard"), {
+        position: data.fen,
+        assetsUrl: "/static/cb/assets/",
+        style: { pieces: { file: "pieces/standard.svg" } },
+        extensions: [{ class: PromotionDialog }, { class: Markers }]
+    })
+
+ws.onopen = function (e) {
+    ws.send('OK')
+}
+
+ws.onmessage = function (e) {
+    data = JSON.parse(e.data)
+    game = new Chess(data.fen)
+    board.setPosition(data.fen)
+    board.setOrientation(game.turn())
+
+    document.getElementById('fen').innerHTML = data.fen;
+    console.log(data)
+};
+
+board.enableMoveInput((event) => {
+    if (event.type === INPUT_EVENT_TYPE.validateMoveInput) {
+        var move = { from: event.squareFrom, to: event.squareTo, promotion: 'q' }
+        try { game.move(move) }
+        catch (error) { return false }
+
+        var ispromo = event.squareTo.charAt(1) == '1' || event.squareTo.charAt(1) == 8
+        var ispawn = event.piece.charAt(1) === "p"
+        if (ispromo && ispawn) {
+            game.undo()
+            promotePawn(event, game.turn(), move);
+        }
+        
+        var solution = data.analysis[0].pv[0]
+        var answer = move.from + move.to + (solution.length == 4 ? '' : move.promotion)
+        if (answer != solution) {
+            game.undo()
+            return false
+        }
+        
+        updateStatus()
+
+    } else if (event.type === INPUT_EVENT_TYPE.moveInputFinished) {
+        event.chessboard.setPosition(game.fen(), true)
+    }
+    return true
+})
+
+function promotePawn(event, turn, move) {
+    board.showPromotionDialog(event.squareTo, turn, (result) => {
+        if (result && result.piece) {
+            move.promotion = result.piece.charAt(1)
+            game.move(move)
+            board.setPiece(result.square, result.piece, true)
+        } else {
+            promotePawn(event, turn, move);
+        }
+    });
+}
+
+function updateStatus() {
+    counter += 1
+    console.log('solved', counter)
+    ws.send('OK')
+}
